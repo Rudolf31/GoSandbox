ee78fca05889d6a40af6374ae21bdd59d5205060
unknown <rudolfshevchenko@gmail.com>
2025-09-20 19:26:31 +0300

    my first meating with gin

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,58 @@
 package main
 
 import (
-	"fmt"
 	"interface_lesson/internal/services"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	a := services.NewCalculatorService()
-	num1, num2 := 1, 2
-	fmt.Print(a.Addition(num1, num2), " ", a.Subtraction(num1, num2), " ", a.GetOpperation())
+	calc := services.NewCalculatorService()
+	router := gin.Default()
+
+	router.GET("/add/:num1/:num2", func(c *gin.Context) {
+		num1_str := c.Param("num1")
+		num2_str := c.Param("num2")
+		num1, err := strconv.Atoi(num1_str)
+		if err != nil {
+			c.Status(http.StatusBadRequest)
+			return
+		}
+
+		num2, err := strconv.Atoi(num2_str)
+		if err != nil {
+			c.Status(http.StatusBadRequest)
+			return
+		}
+
+		result := calc.Addition(num1, num2)
+		c.JSON(http.StatusOK, gin.H{"result": result})
+	})
+
+	router.GET("/sub/:num1/:num2", func(c *gin.Context) {
+		num1_str := c.Param("num1")
+		num2_str := c.Param("num2")
+
+		num1, err := strconv.Atoi(num1_str)
+		if err != nil {
+			c.Status(http.StatusBadRequest)
+			return
+		}
+		num2, err := strconv.Atoi(num2_str)
+		if err != nil {
+			c.Status(http.StatusBadRequest)
+			return
+		}
+
+		result := calc.Subtraction(num1, num2)
+		c.JSON(http.StatusOK, gin.H{"result": result})
+	})
+
+	router.GET("/count", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"count": calc.GetOpperation()})
+	})
+
+	router.Run()
 }
