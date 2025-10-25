2871b5fe34ae8810fb49af10735bcf21dc915790
Rudolf31 <rudolfshevchenko@gmail.com>
2025-10-25 21:48:16 +0300

    finished with fx library first meating

--- backend/internal/routes/gin.go
+++ backend/internal/routes/gin.go
@@ -2,8 +2,8 @@ package routes
 
 import (
 	"context"
-	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"go.uber.org/fx"
@@ -11,12 +11,31 @@ import (
 
 func NewGin(lc fx.Lifecycle) *gin.Engine {
 	router := gin.Default()
+
+	router.GET("/test", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"test": "ok"})
+	})
+
 	server := &http.Server{
-		Addr: ":8080",
+		Addr:    ":8080",
 		Handler: router,
 	}
+
 	lc.Append(fx.Hook{
-		OnStart: ,
+		OnStart: func(ctx context.Context) error {
+			go func() {
+				servErr := server.ListenAndServe()
+				if servErr != nil {
+					panic(servErr.Error())
+				}
+			}()
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+			defer cancel()
+			return server.Shutdown(ctx)
+		},
 	})
 
 	return router
